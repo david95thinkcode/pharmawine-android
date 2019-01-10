1334f9c508665b498e48db04b84f4ebe94690737
David K. Hiheaglo <davidhihea@gmail.com>
2019-01-10 16:21:05 +0100

    WIP - Implemeing prospection Activity

--- app/src/main/java/com/jmaplus/pharmawine/activities/ProspectionActivity.java
+++ app/src/main/java/com/jmaplus/pharmawine/activities/ProspectionActivity.java
@@ -1,12 +1,27 @@
 package com.jmaplus.pharmawine.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.jmaplus.pharmawine.R;
+import com.jmaplus.pharmawine.adapters.RemainingClientsAdapter;
+import com.jmaplus.pharmawine.models.Client;
+import com.jmaplus.pharmawine.utils.FakeData;
 
-public class ProspectionActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProspectionActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private LinearLayout mNewClient;
+    private RecyclerView mRecyclerView;
+    private RemainingClientsAdapter mAdapter;
+    private List<Client> clientsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +29,38 @@ public class ProspectionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_prospection);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        bindViewsAndInitilise();
+
+        fetchRemainingClients();
+    }
+
+    private void bindViewsAndInitilise() {
+
+        mNewClient = findViewById(R.id.lay_new_client);
+        mRecyclerView = findViewById(R.id.rv_remaining_customers_prospection);
+        mNewClient.setOnClickListener(this);
+
+        // Initializations
+        clientsList = new ArrayList();
+        mAdapter = new RemainingClientsAdapter(this, clientsList);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayout.VERTICAL, false));
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    private void showNewClientView() {
+        // TODO:
+
+    }
+
+    private void fetchRemainingClients() {
+        // todo : use api datas
+
+        // using fake data
+        for (Client c : FakeData.getMedicalTeamClients()) {
+            clientsList.add(c);
+            mAdapter.notifyItemInserted(clientsList.size() - 1);
+        }
     }
 
     @Override
@@ -26,5 +73,12 @@ public class ProspectionActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == mNewClient.getId()) {
+            showNewClientView();
+        }
     }
 }
