99cc205fcf5204570a7944a631261c84076bfaac
David K. Hiheaglo <davidhihea@gmail.com>
2019-01-15 11:51:17 +0100

    [ Update ] - Missed fragment code implemented especially onAttach and OnDetach

--- app/src/main/java/com/jmaplus/pharmawine/fragments/rapport/ReportEtape1Fragment.java
+++ app/src/main/java/com/jmaplus/pharmawine/fragments/rapport/ReportEtape1Fragment.java
@@ -1,57 +1,108 @@
 package com.jmaplus.pharmawine.fragments.rapport;
 
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
 import com.jmaplus.pharmawine.R;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.jmaplus.pharmawine.utils.Utils;
 
 
 public class ReportEtape1Fragment extends Fragment {
 
-    private Spinner zoneSpinner;
-    private Button btnE1toE2Report;
+    private static final String TAG = "ReportEtape1Fragment";
+    private OnFragmentInteractionListener mListener;
+    private Spinner centreSpinner;
+    private Button nextBtn;
+    private Context mParentContext;
+
+    private String mCentre = "";
 
     public ReportEtape1Fragment() {
-
+        // this constructor should be empty
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
     }
 
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View fre1View = inflater.inflate(R.layout.fragment_report_etape1, container, false);
-        zoneSpinner = fre1View.findViewById(R.id.sp_choix_centre_pour_rapport);
+        View rootView = inflater.inflate(R.layout.fragment_report_etape1, container, false);
 
-        List<String> spinnerArray = new ArrayList<>();
-        spinnerArray.add("Centre1");
-        spinnerArray.add("Centre2");
-        spinnerArray.add("Centre3");
+        mParentContext = requireContext();
 
-//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(fre1View.getContext(), spinnerArray.size(), android.R.layout.simple_spinner_item);
-//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        zoneSpinner.setAdapter(adapter);
+        // binding views
+        centreSpinner = rootView.findViewById(R.id.sp_choix_centre_pour_rapport);
+        nextBtn = rootView.findViewById(R.id.btn_suivant_etape_1);
 
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(mParentContext,
+                R.array.centres_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        centreSpinner.setAdapter(adapter);
 
-        return fre1View;
+        setUpEvents();
 
+        return rootView;
     }
 
+    public void setUpEvents() {
 
+        // events
+        centreSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                mCentre = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!mCentre.isEmpty() || mCentre != null) {
+                    mListener.onStep1Finished(mCentre);
+                } else {
+                    Utils.presentToast(requireContext(),
+                            "Vous devez selectionner le centre",
+                            true);
+                }
+
+            }
+        });
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        void onStep1Finished(String center);
+    }
 }
