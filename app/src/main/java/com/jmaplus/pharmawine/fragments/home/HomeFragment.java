2afaa1952cfc65863ca3f3bdb185949bd0717f68
David K. Hiheaglo <davidhihea@gmail.com>
2019-01-28 02:38:09 +0100

    [ Update ] Nombre de client restant actualise sur la home fragment pour un delegue

--- app/src/main/java/com/jmaplus/pharmawine/fragments/home/HomeFragment.java
+++ app/src/main/java/com/jmaplus/pharmawine/fragments/home/HomeFragment.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +13,7 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.akexorcist.roundcornerprogressbar.RoundCornerProgressBar;
 import com.codetroopers.betterpickers.calendardatepicker.CalendarDatePickerDialogFragment;
@@ -24,19 +24,26 @@ import com.jmaplus.pharmawine.activities.RemainingClientsActivity;
 import com.jmaplus.pharmawine.activities.SeenCustomers;
 import com.jmaplus.pharmawine.models.AuthUser;
 import com.jmaplus.pharmawine.models.AuthenticatedUser;
+import com.jmaplus.pharmawine.models.Customer;
+import com.jmaplus.pharmawine.utils.CustomerCalls;
 
 import java.text.DateFormatSymbols;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
+
+import javax.annotation.Nullable;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment
+        implements CustomerCalls.Callbacks {
 
     private String TAG = "HomeFragment";
     private TextView tvDate, tvNetworkLabel, tvProgress;
+    private TextView tvRemainingCount;
     private LinearLayout cvDate;
     private RoundCornerProgressBar dailyProgressBar;
     private Context mContext;
@@ -68,6 +75,7 @@ public class HomeFragment extends Fragment {
         layBottomFabs = view.findViewById(R.id.lay_bottom_fabs);
         clientRemaining = view.findViewById(R.id.clients_remaining);
         clientSeen = view.findViewById(R.id.clients_seen);
+        tvRemainingCount = view.findViewById(R.id.textView_num_visit_restant);
 
         tvNetworkLabel = view.findViewById(R.id.tv_network_label);
         fabNetwork = view.findViewById(R.id.fab_network);
@@ -78,7 +86,19 @@ public class HomeFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onResume() {
+
+        // Refresh all datas
+        // Remaining clients count
+        CustomerCalls.getRemaining(AuthUser.getToken(mContext), this);
+
+        super.onResume();
+    }
+
     private void initViews() {
+
+        tvRemainingCount.setText(String.valueOf(0));
 
         currentUser = AuthUser.getAuthenticatedUser(requireContext());
         try {
@@ -150,4 +170,35 @@ public class HomeFragment extends Fragment {
         tvProgress.setText(String.valueOf(value).concat("%"));
     }
 
+    @Override
+    public void onCustomerDetailsResponse(@Nullable Customer customer) {
+
+    }
+
+    @Override
+    public void onCustomerDetailsFailure() {
+
+    }
+
+    @Override
+    public void onKnownProspectResponse(@Nullable List<Customer> customers) {
+
+    }
+
+    @Override
+    public void onKnownProspectFailure() {
+
+    }
+
+    @Override
+    public void onRemainingCustomersResponse(@Nullable List<Customer> customers) {
+
+        tvRemainingCount.setText(String.valueOf(customers.size()));
+        Toast.makeText(mContext, "Nombre de client restant mis a jour", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRemainingCustomersFailure() {
+
+    }
 }
